eda15b18438afef775c42757801526e1749bbe7f
BkStyle89 <kajzer@hotmail.it>
2026-04-01 16:32:01 +0200

    array in the routers folder and changed the folder name in routers

--- index.js
+++ index.js
@@ -13,44 +13,7 @@ app.get('/', (req, res) => {
 
 app.use("/posts", postRouter)
 
-// Array of posts
-const posts = [
-  {
-    id: 1,
-    title: 'Post 1',
-    content: 'Content of post 1',
-    image: '/images/post1.jpg',
-    tags: ['tag1', 'tag2']
-  },
-  {
-    id: 2,
-    title: 'Post 2',
-    content: 'Content of post 2',
-    image: '/images/post2.jpg',
-    tags: ['tag3', 'tag4']
-  },
-  {
-    id: 3,
-    title: 'Post 3',
-    content: 'Content of post 3',
-    image: '/images/post3.jpg',
-    tags: ['tag5', 'tag6']
-  },
-  {
-    id: 4,
-    title: 'Post 4',
-    content: 'Content of post 4',
-    image: '/images/post4.jpg',
-    tags: ['tag7', 'tag8']
-  },
-  {
-    id: 5,
-    title: 'Post 5',
-    content: 'Content of post 5',
-    image: '/images/post5.jpg',
-    tags: ['tag9', 'tag10']
-  }
-];
+
 
 // Route to return posts
 app.get('/bacheca', (req, res) => {
